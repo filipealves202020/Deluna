d9625b03f713b6335ee3bbd568a47061d932988a
filipealves202020 <30376482+filipealves202020@users.noreply.github.com>
2025-08-30 11:37:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,25 @@
+// sw.js
 const CACHE_NAME = 'ml-deluna-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/IMG_20250816_204625.jpg',
-  '/style.css',
-  '/script.js'
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
